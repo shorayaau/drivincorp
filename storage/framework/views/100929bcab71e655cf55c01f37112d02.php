28f0b7754b1b10d8e1646c056b0867319e2a2524
shorayaau <127867416+shorayaau@users.noreply.github.com>
2023-03-15 17:07:10 +0700

    Delete storage/framework directory

--- storage/framework/views/100929bcab71e655cf55c01f37112d02.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('judul'); ?>
-Home
-<?php $__env->stopSection(); ?> 
-<?php echo $__env->make('layouts.template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\xampp\htdocs\company\resources\views/homepage.blade.php ENDPATH**/ ?>
